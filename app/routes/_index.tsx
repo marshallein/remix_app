f087ae5a19fb4ccc686723a1591f78df45887b0c
marshallein <72290795+marshallein@users.noreply.github.com>
2024-12-06 15:31:06 +0700

    Update _index.tsx

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -153,30 +153,34 @@ export default function Index() {
          <section className="on-sales my-5">
             <div className="container">
                <h2 className="on-sales-title">ON SALES</h2>
-               {onSaleProduct.map((product, idx) => (
-                  <div
-                     className="col-md-3"
-                     key={idx}
-                     id={String(product.id)}
-                     tabIndex={0}
-                     onKeyDown={() => {}}
-                     role="button"
-                     onClick={handleOnClickProduct}
-                  >
-                     <div className="product-card">
-                        <img
-                           src={product.mainImageString || IMAGE_FALL_BACK_URL}
-                           alt="Product 1"
-                           className="img-fluid"
-                        />
+               <div className="row container mt-4 align-items-center">
+                  {onSaleProduct.map((product, idx) => (
+                     <div
+                        className="col-md-3"
+                        key={idx}
+                        id={String(product.id)}
+                        tabIndex={0}
+                        onKeyDown={() => {}}
+                        role="button"
+                        onClick={handleOnClickProduct}
+                     >
+                        <div className="product-card">
+                           <img
+                              src={
+                                 product.mainImageString || IMAGE_FALL_BACK_URL
+                              }
+                              alt="Product 1"
+                              className="img-fluid"
+                           />
+                        </div>
+                        <p style={{ textAlign: 'center', fontSize: '16px' }}>
+                           {product.productName}
+                           <br />
+                           {product.price.toLocaleString()}$
+                        </p>
                      </div>
-                     <p>
-                        {product.productName}
-                        <br />
-                        {product.price.toLocaleString()}$
-                     </p>
-                  </div>
-               ))}
+                  ))}
+               </div>
             </div>
          </section>
 
@@ -184,31 +188,66 @@ export default function Index() {
             <div className="container">
                <h2 className="feedback-title">FEEDBACK</h2>
 
-               <div className="row text-center">
+               <div className="row text-center mt-4">
                   <div className="col-md-4">
                      <div className="feedback-card">
-                        <div className="circle"></div>
-                        <p>NAME</p>
-                        <p>★★★★★</p>
-                        <p>Feedback text goes here.</p>
+                        <div className="circle">
+                           <img
+                              className="img-thumbnail"
+                              src="https://api.dicebear.com/9.x/lorelei/svg"
+                              alt="avAtar"
+                           />
+                        </div>
+                        <p className="customer-name">Naima Merlyn</p>
+                        <p className="customer-country">Hawaiian</p>
+                        <p className="customer-feedback">
+                           Absolutely loved my new áo dài! The fabric feels
+                           luxurious, and the embroidery is stunning. It fits
+                           perfectly and made me feel so elegant at my
+                           sister&apos;s wedding. Highly recommend this store
+                           for their attention to detail and quality!
+                        </p>
                      </div>
                   </div>
 
                   <div className="col-md-4">
                      <div className="feedback-card">
-                        <div className="circle"></div>
-                        <p>NAME</p>
-                        <p>★★★★</p>
-                        <p>Feedback text goes here.</p>
+                        <div className="circle">
+                           <img
+                              className="img-thumbnail"
+                              src="https://api.dicebear.com/9.x/lorelei/svg"
+                              alt="avAtar"
+                           />
+                        </div>
+                        <p className="customer-name">Bethsabée Saldís</p>
+                        <p className="customer-country">Greek</p>
+                        <p className="customer-feedback">
+                           The áo dài I bought is beautiful, but the size runs a
+                           little smaller than expected. Thankfully, the staff
+                           was very helpful with the exchange process. Overall,
+                           happy with the design and service.
+                        </p>
                      </div>
                   </div>
 
                   <div className="col-md-4">
                      <div className="feedback-card">
-                        <div className="circle"></div>
-                        <p>NAME</p>
-                        <p>★★★</p>
-                        <p>Feedback text goes here.</p>
+                        <div className="circle">
+                           <img
+                              className="img-thumbnail"
+                              src="https://api.dicebear.com/9.x/lorelei/svg"
+                              alt="avAtar"
+                           />
+                        </div>
+                        <p className="customer-name">Viona Myrthe</p>
+                        <p className="customer-country">German</p>
+                        <p className="customer-feedback">
+                           I couldn&apos;t be happier with my purchase! The
+                           color and pattern of the áo dài exceeded my
+                           expectations, and it arrived in perfect condition.
+                           It&apos;s clear they care about their customers and
+                           their products. Will definitely shop here again!
+                        </p>
                      </div>
                   </div>
                </div>
